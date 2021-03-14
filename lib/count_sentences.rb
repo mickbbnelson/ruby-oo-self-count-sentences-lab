f56acbcc32172aa6f46817d388154a63165917dd
Mick Nelson <mickbbnelson@gmail.com>
2021-03-14 13:31:07 -0700

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
-require 'pry'
+require "pry"
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    delimiters = [".", "?", "!!", "..."]
+    self.split(Regexp.union(delimiters)).count
   end
 end
